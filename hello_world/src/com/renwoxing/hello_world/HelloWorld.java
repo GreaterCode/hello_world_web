60edd5a5c5786de1e2a242969a8cce4dc0172758
yuanting <yuanting@agree.com.cn>
2018-07-28 03:47:43 +0800

    Initial commit

---
+++ hello_world/src/com/renwoxing/hello_world/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.renwoxing.hello_world;
+
+/**
+ * Created by renwoxing on 2018/7/28.
+ */
+public class HelloWorld {
+    public static void main(String []args){
+        System.out.print("Hello world");
+
+    }
+}
